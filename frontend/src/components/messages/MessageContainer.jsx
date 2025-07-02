0624f3e6aa00be3cba59c64f36df19f7bcbda749
Mohit Yadav <mohit180204@gmail.com>
2025-07-02 16:36:09 +0530

    Socket for real time has been implemented

--- frontend/src/components/messages/MessageContainer.jsx
+++ frontend/src/components/messages/MessageContainer.jsx
@@ -3,10 +3,12 @@ import useConversation from "../../zustand/useConversation";
 import MessageInput from "./MessageInput";
 import Messages from "./Messages";
 import { TiMessages } from "react-icons/ti";
+import { useAuthContext } from "../../context/AuthContext";
+import useListenMessages from "../../hooks/useListenMessages";
 
 const MessageContainer = () => {
 const { selectedConversation, setSelectedConversation } = useConversation();
-
+	useListenMessages();
 	useEffect(() => {
 		// cleanup function (unmounts)
 		return () => setSelectedConversation(null);
@@ -36,11 +38,11 @@ const { selectedConversation, setSelectedConversation } = useConversation();
 export default MessageContainer;
 
 const NoChatSelected = () => {
-
+    const { authUser } = useAuthContext();
 	return (
 		<div className='flex items-center justify-center w-full h-full'>
 			<div className='px-4 text-center sm:text-lg md:text-xl text-gray-200 font-semibold flex flex-col items-center gap-2'>
-				<p>Welcome 👋 Mohitt ❄</p>
+				<p>Welcome 👋  {authUser.fullName} ❄</p>
 				<p>Select a chat to start messaging</p>
 				<TiMessages className='text-3xl md:text-6xl text-center' />
 			</div>
