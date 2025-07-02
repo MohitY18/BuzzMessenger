9c233071416108d0d41b43bc3bcb71adf5dfa58d
Mohit Yadav <mohit180204@gmail.com>
2025-07-02 16:52:19 +0530

    ReadyForDeployment

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+import path from "path";
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
@@ -8,9 +9,12 @@ import userRoutes from "./routes/user.routes.js";
 import connectToMongoDB from "./db/connectToMongoDB.js";
 import {app,server} from './socket/socket.js';
 
+dotenv.config();
+
+const __dirname = path.resolve();
+
 const PORT = process.env.PORT || 5000;
 
-dotenv.config();
 
 app.use(express.json());
 app.use(cookieParser());
@@ -21,6 +25,14 @@ app.use('/api/users',userRoutes);
 // app.get('/',(req,res)=>{
 //      res.send("Hello My Port");
 // })
+
+
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+// app.get("*", (req, res) => {
+// 	res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+// });
+
 
 
 server.listen(PORT,()=>{
